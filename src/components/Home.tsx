08987f2be0f034d2f57620b6bc6d5e7801c92a0b
Yumi Izumi <me@yumiizumi.com>
2023-12-09 16:28:04 -0500

    Add room code input and conditional rendering of buttons in Home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,28 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 const Home = () => {
+  const [roomCode, setRoomCode] = useState<string>("");
+
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-64 gap-2 flex flex-col  ">
-        <Input className="w-full" type="text" placeholder="Enter a room code" />
-        <Button className="w-full">Create a new room</Button>
-        <Button className="w-full">Enter a random room</Button>
+        <Input
+          className="w-full"
+          value={roomCode}
+          onChange={(e) => setRoomCode(e.target.value)}
+          type="text"
+          placeholder="Enter a room code"
+        />
+        {roomCode ? (
+          <Button className="w-full">Enter room</Button>
+        ) : (
+          <>
+            <Button className="w-full">Create a new room</Button>
+            <Button className="w-full">Enter a random room</Button>
+          </>
+        )}
       </div>
     </div>
   );
