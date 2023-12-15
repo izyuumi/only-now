5d2132466eb747c09e071eb8d221027d78e55e26
Yumi Izumi <me@yumiizumi.com>
2023-12-15 17:04:40 +0900

    Real-time chatting update

--- app/chat/[room]/page.tsx
+++ app/chat/[room]/page.tsx
@@ -4,19 +4,27 @@ import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/use-toast";
 import { cacheRoomAndUser, getUserFromRoom } from "@/utils";
 import { createClient } from "@/utils/supabase/client";
-import { ChevronLeft, Share, Unlock, Lock } from "lucide-react";
+import { ChevronLeft, Lock, Share, Unlock } from "lucide-react";
 import { useEffect, useState } from "react";
+import { z } from "zod";
 
-interface Message {
-  user_id: string;
-  message: string;
-}
+const MessageSchema = z.object({
+  user_id: z.string(),
+  message: z.string(),
+});
+type Message = z.infer<typeof MessageSchema>;
 
-interface User {
-  user_id: string;
-  user_index: number;
-  message: string;
-}
+const UserSchema = z.object({
+  user_index: z.number(),
+  message: z.string(),
+});
+type User = z.infer<typeof UserSchema>;
+
+const NewUserMessageSchema = z.object({
+  user_id: z.string(),
+  user_index: z.number(),
+});
+type NewUserMessage = z.infer<typeof NewUserMessageSchema>;
 
 export default function Chat({ params }: { params: { room: string } }) {
   const supabase = createClient();
@@ -24,9 +32,8 @@ export default function Chat({ params }: { params: { room: string } }) {
 
   const channel = supabase.channel(`room:${params.room}`);
   const [myUuid, setMyUuid] = useState("");
-  const [otherMessage, setOtherMessage] = useState("");
   const [isPrivate, setIsPrivate] = useState(true);
-  const [users, setUsers] = useState<User[]>([]);
+  const [users, setUsers] = useState<Record<string, User>>({});
   const [myMessage, setMyMessage] = useState("");
 
   const checkCache = () => {
@@ -53,35 +60,53 @@ export default function Chat({ params }: { params: { room: string } }) {
     subscribeToRoom();
   };
 
+  const sendJoinMessage = async () => {
+    if (!myUuid) return;
+    channel.send({
+      type: "broadcast",
+      event: "new_user",
+      payload: {
+        user_id: myUuid,
+        user_index: Object.keys(users).length,
+      },
+    });
+  };
+
   const subscribeToRoom = async () => {
     channel
-      .on("broadcast", { event: "message" }, ({ payload, event }) => {
-        console.log(payload);
-        if (payload.newMessage) {
+      .on("broadcast", { event: "message" }, ({ payload }) => {
+        if (MessageSchema.safeParse(payload.newMessage).success) {
           const newMes = payload.newMessage as Message;
-          const newMesUser = users.find(
-            (user) => user.user_id == newMes.user_id
-          );
-
-          if (newMesUser) {
-            newMesUser.message = newMes.message;
-            setUsers((prev) => [
-              ...prev.filter((user) => user.user_id !== newMes.user_id),
-              newMesUser,
-            ]);
-          } else {
-            setUsers((prev) => [
-              ...prev,
-              {
-                user_id: newMes.user_id,
-                message: newMes.message,
-                user_index: 2,
-              },
-            ]);
-          }
+          setUsers((prev) => ({
+            ...prev,
+            [newMes.user_id]: {
+              message: newMes.message,
+              user_index: prev[newMes.user_id]?.user_index ?? 0,
+            },
+          }));
         }
       })
-      .subscribe((status) => console.log("subscribed to room", status));
+      .on("broadcast", { event: "new_user" }, ({ payload }) => {
+        if (NewUserMessageSchema.safeParse(payload).success) {
+          const newUser = payload as NewUserMessage;
+          setUsers((prev) => ({
+            ...prev,
+            [newUser.user_id]: {
+              message: "",
+              user_index: newUser.user_index,
+            },
+          }));
+        }
+      })
+      .subscribe((status) => {
+        if (status === "SUBSCRIBED") {
+          toast({
+            title: "Connected to room",
+            description: "You can now chat in this room",
+            className: "bg-green-100",
+          });
+        }
+      });
   };
 
   useEffect(() => {
@@ -120,12 +145,12 @@ export default function Chat({ params }: { params: { room: string } }) {
     if (isPrivate) {
       toast({
         title: "Private room",
-        description: "This room is now private",
+        description: "This room is now private. New random users cannot join.",
       });
     } else {
       toast({
         title: "Public room",
-        description: "This room is now public",
+        description: "This room is now public. Random users can join.",
       });
     }
   };
@@ -155,14 +180,16 @@ export default function Chat({ params }: { params: { room: string } }) {
         {isPrivate ? <Unlock aria-hidden /> : <Lock aria-hidden />}
       </Button>
       <div className="flex flex-col gap-2">
-        {users.map((user) => (
-          <CustomTextarea
-            key={user.user_id}
-            who="other"
-            message={user.message}
-            user_index={user.user_index}
-          />
-        ))}
+        {Object.entries(users)
+          .filter(([user_id]) => user_id !== myUuid)
+          .map(([user_id, user]) => (
+            <CustomTextarea
+              key={user_id}
+              who="other"
+              message={user.message}
+              user_index={user.user_index}
+            />
+          ))}
         <CustomTextarea
           who="me"
           message={myMessage}
@@ -202,8 +229,7 @@ const CustomTextarea = (props: TextareaProps) => {
       onChange={(e) => isMe && props.setMessage(e.target.value)}
       className="outline-none resize-none text-center text-xl bg-transparent"
       placeholder={isMe ? "Type here" : "Waiting for a message..."}
-    >
-      {message}
-    </textarea>
+      defaultValue={""}
+    />
   );
 };
