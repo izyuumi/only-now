083bf21d97627623147dff5e11a6e4e9e8c6eb3e
Yumi Izumi <me@yumiizumi.com>
2023-12-16 15:31:01 +0900

    Add meta tags for social media preview image

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./font.css";
 import "./bg.css";
 import "./main.css";
 import { Toaster } from "@/components/ui/toaster";
+import Head from "next/head";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -22,6 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={GeistSans.className}>
+      <Head>
+        <meta property="og:image" content="/preview.png" />
+        <meta property="twitter:image" content="/preview.png" />
+      </Head>
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">
           {children}
