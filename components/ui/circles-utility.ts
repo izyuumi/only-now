e3248080213153ee59f164fcb15218dc2edef37c
Yumi Izumi <me@yumiizumi.com>
2023-12-19 10:46:59 +0900

    Remove unused code

--- components/ui/circles-utility.ts
+++ components/ui/circles-utility.ts
@@ -1,24 +1,19 @@
-// circlesUtility.ts
-
 export function generateCircle(): void {
-    const numCircles = 1; 
-    const container = document.getElementById('box-container');
-  
-    if (container) {
-      for (let i = 1; i <= numCircles; i++) {
-        console.log("making circle");
-        const li: HTMLLIElement = document.createElement('li');
-        li.style.left = `${Math.random() * 100}%`;
-        li.style.width = `${Math.random() * 50}px`;
-        li.style.height = li.style.width;
-        li.style.animationDuration = `${Math.random() * 20 + 5}s`; 
-        li.style.animationDelay = `0s`;
-  
-        li.classList.add('box');
-  
-        // Add event listener for animation iteration
-        container.appendChild(li);
-      }
+  const numCircles = 1;
+  const container = document.getElementById("box-container");
+
+  if (container) {
+    for (let i = 1; i <= numCircles; i++) {
+      const li: HTMLLIElement = document.createElement("li");
+      li.style.left = `${Math.random() * 100}%`;
+      li.style.width = `${Math.random() * 50}px`;
+      li.style.height = li.style.width;
+      li.style.animationDuration = `${Math.random() * 20 + 5}s`;
+      li.style.animationDelay = `0s`;
+
+      li.classList.add("box");
+
+      container.appendChild(li);
     }
   }
-  
+}
